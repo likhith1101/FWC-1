1cf8a397cc42583ab93875120a35aeb891211e1d
dunnvamsi <119913502+dunnvamsi@users.noreply.github.com>
2023-01-05 12:47:56 +0530

    Add files via upload

---
+++ avr_gcc/src/avr_gcc/codes/main.c
@@ -0,0 +1,26 @@
+#include<avr/io.h>
+#include<stdbool.h>
+#include<util/delay.h>
+int main(void)
+{
+	int X,Y,Z;
+	bool F;
+
+	DDRB	|=0b11111111;
+	PORTD=0xFC;;
+	DDRB=0b00100000;
+	
+	while(1)
+	{
+		X=(PIND &(1<<PIND2))==(1<< PIND2);
+		Y=(PIND &(1<<PIND3))==(1<< PIND3);
+		Z=(PIND &(1<<PIND4))==(1<< PIND4);
+
+		  F=!(Z||!(X||Y));
+
+		PORTB=(F<<5);
+
+	}
+	return 0;
+}
+
