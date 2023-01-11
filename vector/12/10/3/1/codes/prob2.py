f6778b4b8ea8855d8eacaacaad78a4bf64085e81
dunnvamsi <119913502+dunnvamsi@users.noreply.github.com>
2023-01-11 17:03:35 +0530

    Add files via upload

---
+++ vector/12/10/3/1/codes/prob2.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
+import math
+import sys     #for path to external scripts
+
+#Two aray vectors are given 
+
+#Formula of an 
+x=math.sqrt(6)/(math.sqrt(3)*2)
+p=math.acos(x)
+w=p*(180/math.pi)
+print(w)
+
+
