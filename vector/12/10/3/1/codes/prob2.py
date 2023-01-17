d811322b49d1e8c4800adbf8da2aa2a30ad38a1b
dunnvamsi <119913502+dunnvamsi@users.noreply.github.com>
2023-01-17 16:37:44 +0530

    Add files via upload

--- vector/12/10/3/1/codes/prob2.py
+++ vector/12/10/3/1/codes/prob2.py
@@ -7,9 +7,9 @@ import sys     #for path to external scripts
 #Two aray vectors are given 
 
 #Formula of an 
-x=math.sqrt(6)/(math.sqrt(3)*2)
-p=math.acos(x)
-w=p*(180/math.pi)
+x=np.sqrt(6)/(np.sqrt(3)*2)
+p=np.arccos(x)
+w=p*(180/np.pi)
 print(w)
 
 
